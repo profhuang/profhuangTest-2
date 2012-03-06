056ebdfa44672f3faaffd19bf9b99ac89f8671bc
swordwu-dell <swordwu-dell@172.19.123.249>
2012-03-06 10:15:28 +0900

    kkk

--- gen/kddi/kddilabs/uig2/R.java
+++ gen/kddi/kddilabs/uig2/R.java
@@ -9,7 +9,7 @@ package kddi.kddilabs.uig2;
 
 public final class R {
     public static final class array {
-        public static final int planets=0x7f050000;
+        public static final int planets=0x7f060000;
     }
     public static final class attr {
     }
@@ -17,37 +17,41 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070006;
-        public static final int checkBox1=0x7f070008;
-        public static final int editText1=0x7f070004;
-        public static final int imageButton1=0x7f070005;
-        public static final int imageView1=0x7f07000d;
-        public static final int menuGroup_Main=0x7f07000e;
-        public static final int menu_clear=0x7f070011;
-        public static final int menu_dial=0x7f070012;
-        public static final int menu_show_browser=0x7f070014;
-        public static final int menu_test=0x7f070013;
-        public static final int menu_testGetContent=0x7f070010;
-        public static final int menu_testPick=0x7f07000f;
-        public static final int radio0=0x7f07000a;
-        public static final int radio1=0x7f07000b;
-        public static final int radio2=0x7f07000c;
-        public static final int radioGroup1=0x7f070009;
-        public static final int ratingBar1=0x7f070001;
-        public static final int seekBar1=0x7f070007;
-        public static final int spinner1=0x7f070000;
-        public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f070002;
+        public static final int button1=0x7f080006;
+        public static final int button2=0x7f08000d;
+        public static final int button3=0x7f08000e;
+        public static final int checkBox1=0x7f080008;
+        public static final int editText1=0x7f080004;
+        public static final int imageButton1=0x7f080005;
+        public static final int menuGroup_Main=0x7f08000f;
+        public static final int menu_clear=0x7f080012;
+        public static final int menu_dial=0x7f080013;
+        public static final int menu_show_browser=0x7f080015;
+        public static final int menu_test=0x7f080014;
+        public static final int menu_testGetContent=0x7f080011;
+        public static final int menu_testPick=0x7f080010;
+        public static final int radio0=0x7f08000a;
+        public static final int radio1=0x7f08000b;
+        public static final int radio2=0x7f08000c;
+        public static final int radioGroup1=0x7f080009;
+        public static final int ratingBar1=0x7f080001;
+        public static final int seekBar1=0x7f080007;
+        public static final int spinner1=0x7f080000;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int second=0x7f030001;
     }
     public static final class menu {
-        public static final int my_menu=0x7f060000;
+        public static final int my_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class xml {
+        public static final int chkbox=0x7f040000;
     }
 }
--- src/kddi/kddilabs/uig2/secondActivity.java
+++ src/kddi/kddilabs/uig2/secondActivity.java
@@ -3,12 +3,20 @@ package kddi.kddilabs.uig2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class secondActivity  extends Activity {
+public class secondActivity  extends Activity implements OnInitListener {
 
+	private static final int REQ_TTS_STATUS_CHECK = 0; //”CˆÓ‚Ì’l
+	private TextToSpeech mTts;
+	private Button btn3;
+	private static final String TAG = "TTS Demo";
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
@@ -21,11 +29,82 @@ public class secondActivity  extends Activity {
 		    }        
 	    });
         
+        Button btn2 = (Button)this.findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		      	Intent intent = new Intent(secondActivity.this, CheckBoxPreferenceActivity.class);
+		      	startActivity(intent);
+		    }        
+	    });
+        
+        btn3 = (Button)this.findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		    	mTts.speak("What can I do for you?", TextToSpeech.QUEUE_ADD, null);
+		    }        
+	    });
+        btn3.setEnabled(false);
+        
+        //Check to be sure that TTS exists and is okay to use
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, REQ_TTS_STATUS_CHECK);
+        
+        //Show the Param
         Bundle bundle = this.getIntent().getExtras();
         String name = bundle.getString("name");
         int age = bundle.getInt("age"); 
         
         Toast.makeText(this, "Call by '" + name + "'" + ", Age is:" + age, Toast.LENGTH_SHORT).show();
     }
-            
+    
+    //startActivityForResult->Call onActivityResult();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	
+    		if (requestCode == REQ_TTS_STATUS_CHECK) {
+	    		switch (resultCode) {
+	    		case TextToSpeech.Engine.CHECK_VOICE_DATA_PASS:
+		    		// TTS is up and running
+		    		mTts = new TextToSpeech(this, this);
+		    		Log.v(TAG, "Pico is installed okay");
+		    		break;
+	    		case TextToSpeech.Engine.CHECK_VOICE_DATA_BAD_DATA:
+	    		case TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_DATA:
+	    		case TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_VOLUME:
+		    		// missing data, install it
+		    		Log.v(TAG, "Need language stuff: " + resultCode);
+		    		Intent installIntent = new Intent();
+		    		installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+		    		startActivity(installIntent);
+		    		break;
+	    		case TextToSpeech.Engine.CHECK_VOICE_DATA_FAIL:
+	    		default:
+	    			Log.e(TAG, "Got a failure. TTS not available");
+	    		}
+    		} else {
+    		// Got something else
+    		}
+    }
+    
+    //mTts = new TextToSpeech(this, this)->active onInit()
+    public void onInit(int status) {
+    	// Now that the TTS engine is ready, we enable the button
+    	if( status == TextToSpeech.SUCCESS) {
+    		btn3.setEnabled(true);
+    	}
+    }    
+    
+    @Override
+    public void onPause() {
+	    super.onPause();
+	    
+	    if( mTts != null)
+	    	mTts.stop();
+	 }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy(); 
+        mTts.shutdown();
+    } 
 }
