07fe05f4399a4de14283eae71e21534bcf39ef5f
swordwu-dell <swordwu-dell@172.19.123.249>
2012-03-06 10:17:10 +0900

    jjj

---
+++ src/kddi/kddilabs/uig2/CheckBoxPreferenceActivity.java
@@ -0,0 +1,25 @@
+package kddi.kddilabs.uig2;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class CheckBoxPreferenceActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Sample of Preference
+		addPreferencesFromResource(R.xml.chkbox);
+		//PreferenceManager.setDefaultValues(this, R.xml.flightoptions, false);
+		
+		//Sample of Get Preference
+		getPreferenceManager();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		boolean option = prefs.getBoolean("show_price_column_pref", false);
+		boolean option2 = prefs.getBoolean("tomato_selection_pref", false);
+		Toast.makeText(this, "Price is '" + option + "'" + ", Tomato is '" + option2 + "'", Toast.LENGTH_SHORT).show();
+	}
+}
